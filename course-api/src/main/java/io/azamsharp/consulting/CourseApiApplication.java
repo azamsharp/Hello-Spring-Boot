3813483c82d2033d08961fb0fa76ec173fba129f
azamsharp <azamsharp@gmail.com>
2017-05-02 22:30:38 -0500

    first commit

---
+++ course-api/src/main/java/io/azamsharp/consulting/CourseApiApplication.java
@@ -0,0 +1,16 @@
+package io.azamsharp.consulting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SpringApplication.run(CourseApiApplication.class, args); 
+
+	}
+
+}
---
+++ course-api/src/main/java/io/azamsharp/consulting/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package io.azamsharp.consulting.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String hi() {
+		return "Hello";
+	}
+}
